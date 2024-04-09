6d07583f40f1ef8be59662c460bdcfd268a76c36
AlexSerg01 <alex.box.work@icloud.com>
2024-04-09 19:39:14 +0200

    1

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import './js/header';
+import './js/hero';
+import './js/about_me';
+import './js/benefits';
+import './js/projects';
+import './js/faq';
+import './js/covers';
+import './js/reviews';
+import './js/work_tougether';

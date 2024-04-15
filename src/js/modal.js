f9f3933f9d750d1f698061efd8825a75273085a7
Hrechaniuk1 <hrechaniuk.a@gmail.com>
2024-04-15 20:50:49 +0300

    modal js updates

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,44 +1,54 @@
-// const modal = document.getElementById('myModal');
-
-// const btn = document.querySelector('button');
-
-// const span = document.getElementsByClassName('close')[0];
-
-// btn.onclick = function () {
-//   modal.style.display = 'block';
-// };
-
-// span.onclick = function () {
-//   modal.style.display = 'none';
-// };
-
-// window.onclick = function (event) {
-//   if (event.target == modal) {
-//     modal.style.display = 'none';
-//   }
-// };
-
-// document.onkeydown = function (event) {
-//   if (event.key === 'Escape') {
-//     modal.style.display = 'none';
-//   }
-// };
 
 const MODAL = document.querySelector(".ModalMenu")
 const OPEN_BTN = document.querySelector("#openModal")
 const CLOSE_BTN = document.querySelector(".CloseModalBtn")
 const BODY = document.querySelector("body")
+const MOB_MENU_LINKS = document.querySelector(".Menu")
+
+const SECTIONS = {
+  aboutMe: document.querySelector("#about-me"),
+  benefits: document.querySelector("#Benefits"),
+  projects: document.querySelector(".SectionProject"),
+  faq: document.querySelector("#FAQ"),
+  order: document.querySelector("#WorkTogether")
+}
+
+// =============================================================
 
 OPEN_BTN.addEventListener("click", doOpenMenu)
 CLOSE_BTN.addEventListener("click", doCloseModal)
+MOB_MENU_LINKS.addEventListener("click", doLinkDIrect)
 
+// =============================================================
 
 function doOpenMenu() {
-  MODAL.classList.add("ModalOpen")
+  MODAL.classList.add("Open")
   BODY.classList.add("ModalBodyLock")
 }
 
 function doCloseModal() {
-  MODAL.classList.remove("ModalOpen")
+  MODAL.classList.remove("Open")
   BODY.classList.remove("ModalBodyLock")
+}
+
+function doLinkDIrect(event) {
+  if (event.target === event.currentTarget || event.target === CLOSE_BTN) {
+  return
+  }
+  if (event.target.classList.contains("AboutMe")) {
+    doCloseModal()
+    SECTIONS.aboutMe.scrollIntoView({behavior: "smooth"})
+  } else if (event.target.classList.contains("Benefits")) {
+    doCloseModal()
+    SECTIONS.benefits.scrollIntoView({behavior: "smooth"})
+  } else if (event.target.classList.contains("Projects")) {
+    doCloseModal()
+    SECTIONS.projects.scrollIntoView({behavior: "smooth"})
+  } else if (event.target.classList.contains("FAQ")) {
+    doCloseModal()
+    SECTIONS.faq.scrollIntoView({behavior: "smooth"})
+  } else if (event.target.classList.contains("OrderButton")) {
+    doCloseModal()
+    SECTIONS.order.scrollIntoView({behavior: "smooth"})
+  }
 }

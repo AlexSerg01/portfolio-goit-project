f0ffa8300533563692fd81185deb517e9d997344
Hrechaniuk1 <hrechaniuk.a@gmail.com>
2024-04-15 19:17:03 +0300

    modal changes

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,25 +1,42 @@
-const modal = document.getElementById('myModal');
+// const modal = document.getElementById('myModal');
 
-const btn = document.querySelector('button');
+// const btn = document.querySelector('button');
 
-const span = document.getElementsByClassName('close')[0];
+// const span = document.getElementsByClassName('close')[0];
 
-btn.onclick = function () {
-  modal.style.display = 'block';
-};
+// btn.onclick = function () {
+//   modal.style.display = 'block';
+// };
 
-span.onclick = function () {
-  modal.style.display = 'none';
-};
+// span.onclick = function () {
+//   modal.style.display = 'none';
+// };
 
-window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = 'none';
-  }
-};
+// window.onclick = function (event) {
+//   if (event.target == modal) {
+//     modal.style.display = 'none';
+//   }
+// };
 
-document.onkeydown = function (event) {
-  if (event.key === 'Escape') {
-    modal.style.display = 'none';
-  }
-};
+// document.onkeydown = function (event) {
+//   if (event.key === 'Escape') {
+//     modal.style.display = 'none';
+//   }
+// };
+
+const MODAL = document.querySelector(".ModalMenu")
+const OPEN_BTN = document.querySelector("#openModal")
+const CLOSE_BTN = document.querySelector(".CloseModalBtn")
+
+OPEN_BTN.addEventListener("click", doOpenMenu)
+CLOSE_BTN.addEventListener("click", doCloseModal)
+
+
+function doOpenMenu() {
+  MODAL.classList.add("ModalOpen")
+}
+
+function doCloseModal() {
+  MODAL.classList.remove("ModalOpen")
+  console.log("hi")
+}
